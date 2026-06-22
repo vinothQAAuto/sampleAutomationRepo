67f6f80ed5b7335ad0963aac0abe680363e8e9c2
vinothQAAuto <vinothsoftware1993@gmail.com>
2026-06-22 08:16:09 +0530

    I have update the firstselenium file

--- src/firstSelenium.java
+++ src/firstSelenium.java
@@ -4,6 +4,11 @@ public class firstSelenium {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int a = 10;
+		int b =20;
+		int c = a+b;
+		System.out.println(c);
+
 	}
 
 }
